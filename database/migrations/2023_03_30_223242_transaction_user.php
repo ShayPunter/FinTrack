599f210b98365ac0e1aaa1a06ddd40f8f365d532
Shay Punter <icodinqs@gmail.com>
2023-03-30 23:34:09 +0100

    chore: changed data type

--- database/migrations/2023_03_30_223242_transaction_user.php
+++ database/migrations/2023_03_30_223242_transaction_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            $table->string('user');
+            $table->integer('user');
         });
     }
 
